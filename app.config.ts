bc889435e4414dc650a38da7ea816c566e2357a4
David Christian <david.w.christian@gmail.com>
2025-05-04 20:01:07 -0700

    Fixed websocket error

--- app.config.ts
+++ app.config.ts
@@ -4,17 +4,27 @@ import { defineConfig } from '@tanstack/react-start/config';
 
 const vite: InlineConfig = {
 	server: {
-		host: 'local.nestql.com',
 		allowedHosts: ['local.nestql.com'],
 		hmr: {
-			protocol: 'wss',
-			host: 'local.nestql.com',
+			protocol: 'ws',
+			host: 'localhost',
 		},
 	},
 	plugins: [
 		viteTsConfigPaths({
 			projects: ['./tsconfig.json'],
 		}),
+		{
+			name: 'report-hmr-ports',
+			configureServer: ({ config }) => {
+				const hmr = config.server.hmr;
+				if (typeof hmr === 'object' && 'port' in hmr) {
+					console.log(
+						`\x1b[34mHMR\x1b[0m is listening to \x1b[32mhttp://localhost:${hmr.port}\x1b[0m`,
+					);
+				}
+			},
+		},
 	],
 };
 
--- src/features/root/components/RootDocument.tsx
+++ src/features/root/components/RootDocument.tsx
@@ -1,7 +1,7 @@
 import { Scripts } from '@tanstack/react-start';
 import { HeadContent } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const RootDocument = ({ children }: { children: React.ReactNode }) => {
 	return (
@@ -11,8 +11,8 @@ export const RootDocument = ({ children }: { children: React.ReactNode }) => {
 			</head>
 			<body className="min-w-full min-h-full">
 				{children}
+				<ReactQueryDevtools />
 				<TanStackRouterDevtools position="bottom-right" />
-				{/* <ReactQueryDevtools initialIsOpen={true} /> */}
 				<Scripts />
 			</body>
 		</html>
