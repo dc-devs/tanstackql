6eafbb9e0de883eb493d3fe8433cc9a9047f8a60
David Christian <david.w.christian@gmail.com>
2025-07-17 18:38:26 -0700

    Fixed WS/HMR support

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	server: {
-		port: 3000,
+		host: '0.0.0.0',
+		port: 3003,
+		allowedHosts: ['localhost', 'local.nestql.com'],
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost',
+			port: 3003,
+		},
 	},
 	plugins: [
 		tanstackStart({
